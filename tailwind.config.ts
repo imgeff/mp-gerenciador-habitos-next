b28d888b95f3bc4fc54419f881a439cbf8a8fa2b
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2024-04-01 21:12:04 -0300

    chore: Adicionar configurações tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,25 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        sans: 'var(--font-inter)',
+        sans: ["var(--font-inter)"],
+        display: ["var(--font-dosis)"],
       },
+      colors: {
+        emerald: {
+          primary: '#45EDAD',
+          secondary: '#3AC993',
+          third: '#2FA175',
+        },
+        red: {
+          primary: '#F85858',
+        }
+      },
+      screens: {
+        'xs': '320px',
+      },
+      animation: {
+        'pulse-fast': 'pulse 1s linear infinite',
+      }
     }
   },
   plugins: [],
