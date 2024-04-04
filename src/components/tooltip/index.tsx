b7854e93ad5f7ceb69ce41062b108ca3b0530d28
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2024-04-03 22:23:56 -0300

    feat: Criar componente Tooltip

---
+++ src/components/tooltip/index.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { ReactNode } from "react";
+
+type TooltipProps = {
+  text: string;
+  children: ReactNode;
+}
+
+export default function Tooltip({ text, children }: TooltipProps) {
+  return (
+    <div className="relative group">
+      {children}
+      <div className="absolute -left-[390%] bottom-[120%] w-max mb-2 bg-neutral-800 text-white text-center text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition duration-200 ease-in-out">
+        {text}
+      </div>
+    </div>
+  );
+}
