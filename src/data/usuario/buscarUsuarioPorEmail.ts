bbc3b89f36f237a77ae146a0fdda3d25f54193eb
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2024-04-04 20:45:42 -0300

    feat: Criar busca de usuário por email

---
+++ src/data/usuario/buscarUsuarioPorEmail.ts
@@ -0,0 +1,15 @@
+import { database } from '..';
+
+export async function buscarUsuarioPorEmail(email: string) {
+  const usuario = await database.usuario.findFirst({
+    where: { email },
+    select: {
+      id: true,
+      nome: true,
+      email: true,
+      senha: true,
+    }
+  });
+
+  return usuario;
+}
