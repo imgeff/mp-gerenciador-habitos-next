47646591d7a1ca920a8c5932d7192bad19c3e3d4
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2024-04-07 20:48:02 -0300

    feat: Criar action de cadastrar hábito

---
+++ src/actions/habits/cadastrarHabito.ts
@@ -0,0 +1,49 @@
+'use server'
+
+import { criarHabito } from '@/data/habitos/criarHabito'
+import { redirect } from 'next/navigation'
+import { usePayload } from '@/services/auth'
+import { z } from 'zod'
+
+const CadastrarHabitoSchema = z.object({
+  habito: z.string().trim().min(1, 'Não é possível criar um hábito vazio'),
+})
+
+export type CadastrarHabitoState = {
+  errors?: {
+    habito?: string[]
+  }
+  message?: string | null
+}
+
+export async function cadastrarHabito(
+  prevState: CadastrarHabitoState | undefined,
+  formData: FormData,
+) {
+  const camposValidados = CadastrarHabitoSchema.safeParse({
+    habito: formData.get('habito'),
+  })
+
+  if (!camposValidados.success) {
+    return {
+      errors: camposValidados.error.flatten().fieldErrors,
+      message: 'Campos inválidos, falha ao cadastrar hábito',
+    }
+  }
+
+  try {
+    const payload = await usePayload()
+    const habito = {
+      habito: camposValidados.data.habito,
+      idUsuario: payload.id as number,
+    }
+    await criarHabito(habito)
+  } catch (error) {
+    console.error(error)
+    return {
+      message: (error as Error).message,
+    }
+  }
+
+  redirect('/')
+}
